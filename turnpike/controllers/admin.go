1c8dedb31d6e156eca62be665f6aa3c35aa036f0
Jakub Dugovič <jdugovic@redhat.com>
2025-11-24 13:17:43 +0100

    RHINENG-22023: move system delete endpoint to admin api

--- turnpike/controllers/admin.go
+++ turnpike/controllers/admin.go
@@ -3,10 +3,12 @@ package controllers
 import (
 	"app/base/database"
 	"app/base/utils"
+	"app/manager/middlewares"
 	"app/tasks/caches"
 	"app/tasks/cleaning"
 	"app/tasks/repack"
 	sync "app/tasks/vmaas_sync"
+	"errors"
 	"net/http"
 	"regexp"
 	"strconv"
@@ -195,4 +197,77 @@ func CleanAADHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, "cleaning advisory account data")
+}
+
+// @Summary Delete system by inventory id
+// @Description Delete system by inventory id
+// @ID deletesystem
+// @Security RhIdentity
+// @Accept   json
+// @Produce  json
+// @Param    inventory_id    path    string   true "Inventory ID"
+// @Param    rh_account_id   query   string   true "Account ID"
+// @Success 200
+// @Failure 400 {object} 	utils.ErrorResponse
+// @Failure 404 {object} 	utils.ErrorResponse
+// @Failure 500 {object} 	utils.ErrorResponse
+// @Router /systems/{inventory_id} [delete]
+func SystemDeleteHandler(c *gin.Context) {
+	account := c.Query("rh_account_id")
+	if account == "" {
+		utils.LogAndRespBadRequest(c, errors.New("bad request"), "rh_account_id query param not found")
+		return
+	}
+
+	inventoryID := c.Param("inventory_id")
+	if inventoryID == "" {
+		c.JSON(http.StatusBadRequest, utils.ErrorResponse{Error: "inventory_id param not found"})
+		return
+	}
+
+	if !utils.IsValidUUID(inventoryID) {
+		utils.LogAndRespBadRequest(c, errors.New("bad request"), "incorrect inventory_id format")
+		return
+	}
+
+	var systemInventoryID []string
+	db := middlewares.DBFromContext(c)
+	tx := db.Begin()
+
+	defer tx.Rollback()
+
+	err := tx.Set("gorm:query_option", "FOR UPDATE OF system_platform").
+		Table("system_platform").
+		Where("rh_account_id = ?", account).
+		Where("inventory_id = ?::uuid", inventoryID).
+		Pluck("inventory_id", &systemInventoryID).Error
+
+	if err != nil {
+		utils.LogAndRespError(c, err, "could not query database for system")
+		return
+	}
+
+	if len(systemInventoryID) == 0 {
+		utils.LogAndRespNotFound(c, errors.New("no rows returned"), "system not found")
+		return
+	}
+
+	query := tx.Exec("select deleted_inventory_id from delete_system(?::uuid)", systemInventoryID[0])
+
+	if query.Error != nil {
+		utils.LogAndRespError(c, err, "Could not delete system")
+		return
+	}
+
+	if tx.Commit().Error != nil {
+		utils.LogAndRespError(c, err, "Could not delete system")
+		return
+	}
+
+	if query.RowsAffected > 0 {
+		c.Status(http.StatusOK)
+	} else {
+		utils.LogAndRespNotFound(c, errors.New("no rows returned"), "system not found")
+		return
+	}
 }
